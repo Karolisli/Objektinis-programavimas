0f9264b8efba3c56719963c3625e1fecd1437239
Karolis <karolisliesys@gmail.com>
2019-07-23 13:03:42 +0300

    Get'ters and Set'ters

---
+++ 19-07-23/public_html/Karolis.php
@@ -0,0 +1,36 @@
+<?php
+require '../config.php';
+require '../functions/file.php';
+require '../functions/html/builder.php';
+require '../functions/form/core.php';
+
+
+class Drink {
+    private $data;
+    
+    
+}
+?>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Welcome To PHP FightClub!</title>
+        <link rel="stylesheet" href="media/css/normalize.css">
+        <link rel="stylesheet" href="media/css/style.css">
+        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+        <link rel="icon" href="favicon.ico" type="image/x-icon">        
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>    
+        <script src="media/js/app.js"></script>    
+    </head>
+    <body>
+        <!-- $nav Navigation generator -->
+        <?php require '../templates/navigation.tpl.php'; ?>        
+
+
+        <!-- $form HTML generator -->
+<?php require '../templates/form.tpl.php'; ?>
+    </body>
+</html>
+
