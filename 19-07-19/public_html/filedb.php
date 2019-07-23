9ecafb868d9480c1e85f81d7c4b4c3db9d2fd4c1
Karolis <karolisliesys@gmail.com>
2019-07-23 12:55:37 +0300

    getRowsWhere

--- 19-07-19/public_html/filedb.php
+++ 19-07-19/public_html/filedb.php
@@ -1,21 +1,21 @@
 <?php
-
 require '../config.php';
 require '../functions/file.php';
 require '../functions/html/builder.php';
 require '../functions/form/core.php';
 
 class FileDB {
+
     private $file_name;
     private $data;
-    
-    public function __construct($file_name){
+
+    public function __construct($file_name) {
         $this->file_name = $file_name;
     }
 
-    public function load(){
+    public function load() {
         if (file_exists($this->file_name)) {
-            $encoded_string = file_get_contents($this->file_name);	
+            $encoded_string = file_get_contents($this->file_name);
 
             if ($encoded_string !== false) {
                 $this->data = json_decode($encoded_string, true);
@@ -23,107 +23,137 @@ class FileDB {
         }
     }
 
-    public function getData(){
-        if($this->data == null){
+    public function getData() {
+        if ($this->data == null) {
             $this->load();
             return $this->data;
-        }else{
+        } else {
             $this->data;
         }
     }
 
-    public function setData($data_array){
+    public function setData($data_array) {
         $this->data = $data_array;
     }
 
-    public function createTable($table_name){
-        if(!$this->tableExists($table_name)){
+    public function createTable($table_name) {
+        if (!$this->tableExists($table_name)) {
             $this->data[$table_name] = [];
             return true;
         }
-        return false;  
+        return false;
     }
 
-    public function dropTable($table_name){
+    public function dropTable($table_name) {
         unset($this->data[$table_name]);
     }
 
-    public function tableExists($table_name){
-        if(isset($this->data[$table_name])){
+    public function tableExists($table_name) {
+        if (isset($this->data[$table_name])) {
             return true;
-        }else{
+        } else {
             return false;
         }
     }
-    
+
     public function insertRow($table_name, $row, $row_id = null) {
         if ($this->tableExists($table_name)) {
             if ($row_id) {
                 $this->data[$table_name][$row_id] = $row;
             } else {
                 $this->data[$table_name][] = $row;
-            }    
-            return true;       
+            }
+            return true;
         }
         return false;
     }
 
-    public function rowExists($table_name, $row_id){
-        if(isset($this->data[$table_name][$row_id])){
+    public function rowExists($table_name, $row_id) {
+        if (isset($this->data[$table_name][$row_id])) {
             return true;
-        }else{
-            return false;
-        }
-    }
-/**
- * 
- * @param type string $table_name
- * @param type string $row
- * @param type string $row_id
- * @return false or true
- */
-    public function rowInsertIfNotExists($table_name, $row, $row_id){
-        if($this->rowExists($table_name, $row_id)){
-            return false;
-        }else{
-            $this->insertRow($table_name, $row, $row_id);
-        }
-    }
-/**
- * 
- * @param type string $table_name
- * @param type string $row_id
- * @param type string $row
- * @return false or true
- */
-    public function updateRow($table_name, $row_id, $row){
-        if($this->rowExists($table_name, $row_id)){
-            $this->data[$table_name][$row_id][$row] = $row;
-            return true;
-        }else{
+        } else {
             return false;
         }
     }
 
-    public function deleteRow($table_name, $row_id){
-        if($this->rowExists($table_name, $row_id)){
+    /**
+     * 
+     * @param type string $table_name
+     * @param type string $row
+     * @param type string $row_id
+     * @return false or true
+     */
+    public function rowInsertIfNotExists($table_name, $row, $row_id) {
+        if ($this->rowExists($table_name, $row_id)) {
+            return false;
+        } else {
+            $this->insertRow($table_name, $row, $row_id);
+        }
+    }
+
+    /**
+     * 
+     * @param type string $table_name
+     * @param type string $row_id
+     * @param type string $row
+     * @return false or true
+     */
+    public function updateRow($table_name, $row_id, $row) {
+        if ($this->rowExists($table_name, $row_id)) {
+            $this->data[$table_name][$row_id][$row] = $row;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * 
+     * @param type string $table_name
+     * @param type string $row_id
+     * @return delete row or false
+     */
+    public function deleteRow($table_name, $row_id) {
+        if ($this->rowExists($table_name, $row_id)) {
             unset($this->data[$table_name][$row_id]);
             return true;
         }
         return false;
     }
-/**
- * 
- * @param type string $table_name
- * @param type string $row_id
- * @return function or false
- */
-    public function getRow($table_name, $row_id){
-        if($this->rowExists($table_name, $row_id)){
+
+    /**
+     * 
+     * @param type string $table_name
+     * @param type string $row_id
+     * @return getrow or false
+     */
+    public function getRow($table_name, $row_id) {
+        if ($this->rowExists($table_name, $row_id)) {
             return $this->data[$table_name][$row_id];
         }
         return false;
     }
+
+    public function getRowsWhere($table_name, $conditions) {
+        $rows = [];
+
+        foreach ($this->data[$table_name] as $row_id => $row) {
+            $conditions_met = true;
+            foreach ($conditions as $cond_id => $cond_value) {
+                $row_value = $row[$cond_id];
+                if ($row_value != $cond_value) {
+                    $conditions_met = false;
+                    break;
+                }
+            }
+
+            if ($conditions_met) {
+                $rows[] = $row;
+            }
+        }
+        return $rows;
+    }
+
 }
 
 $db = new FileDB(STORAGE_FILE);
@@ -132,8 +162,7 @@ $db->createTable('users');
 
 var_dump($db);
 
-var_dump($db->getRow('users', 'thing'));
-
+var_dump($db->getRowsWhere('users', 'thing'));
 ?>
 
 <html>
@@ -154,7 +183,7 @@ var_dump($db->getRow('users', 'thing'));
 
 
         <!-- $form HTML generator -->
-        <?php require '../templates/form.tpl.php'; ?>
+<?php require '../templates/form.tpl.php'; ?>
     </body>
 </html>
 
